86489310c6a4e454d3f7bc6fb5e667536bfd13d5
dryxoxo <ibrohimhuzaimi@gmail.com>
2022-10-11 01:02:58 +0700

    fix center card

--- TPA-3/utils/cardEmpty.js
+++ TPA-3/utils/cardEmpty.js
@@ -2,7 +2,7 @@ export function emptyCard(title, image, release, rate){
     let listFilm = document.getElementById('listMovie')
     return `
     <div class="col-md-4 d-flex align-items-stretch mb-3">
-        <div class="card" style="width: 18rem;">
+        <div class="card mx-auto" style="width: 18rem;">
             <img src="https://image.tmdb.org/t/p/w185${image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${title}</h5>
