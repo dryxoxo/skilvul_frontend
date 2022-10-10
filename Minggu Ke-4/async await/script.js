e3f9600e26c5306ce7f41987329eed8aa7babbeb
dryxoxo <ibrohimhuzaimi@gmail.com>
2022-10-10 15:38:34 +0700

    source code Promise, Async Await & Fetch

---
+++ Minggu Ke-4/async await/script.js
@@ -0,0 +1,49 @@
+let nonton = (kondisi) => {
+    return new Promise((resolve, reject) => {
+
+        if (kondisi == "jalan") {
+            resolve('nonton terpenuhi')
+        } else {
+            reject('gagal nonton')
+        }
+    })
+}
+
+
+console.log('hai');
+
+nonton("jalan")
+.then(result => {
+    console.log(result);
+}).catch(err => {
+    console.log(err);
+});
+
+async function nama() {
+    try {
+        result = await nonton()
+        console.log(`ini dari try catch ${result}`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+nama()
+
+//ambil data digimon
+
+fetch('https://digimon-api.vercel.app/api/digimon')
+.then(result => result.json())
+.then(result => console.log(result))
+
+getDataDigimon = async () => {
+    try {
+        let result = await fetch('https://digimon-api.vercel.app/api/digimon')
+        result = await result.json()
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getDataDigimon()
