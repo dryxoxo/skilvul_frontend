ba1f760b451aaeb29c3a39a4a123224c47eef5b2
dryxoxo <ibrohimhuzaimi@gmail.com>
2022-11-13 00:56:34 +0700

    finish

--- TPA-4/baim-portofolio/src/main.jsx
+++ TPA-4/baim-portofolio/src/main.jsx
@@ -6,9 +6,7 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </React.StrictMode>
 )
--- TPA-4/baim-portofolio/src/pages/Portofolio.jsx
+++ TPA-4/baim-portofolio/src/pages/Portofolio.jsx
@@ -1,10 +1,13 @@
 import Nav from "../components/Nav";
+import PortofolioCard from "../components/PortofolioCard";
 export default function Portofolio() {
   return (
     <>
       <div className="container">
         <Nav />
-        <h1>Ini Portofolio</h1>
+        <div className="mt-5">
+          <PortofolioCard />
+        </div>
       </div>
     </>
   );
