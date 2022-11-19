875b6ae222387a44f6b7b40060f54c7304281d20
ibrohimhuzaimi <ibrohimhuzaimi@gmail.com>
2022-11-19 22:54:31 +0700

    post threads

--- Group-FE-5/carkus-fe5/src/redux/store/index.js
+++ Group-FE-5/carkus-fe5/src/redux/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import listKampusReducer from "../reducer/listKampusReducer";
+import listThreadReducer from "../reducer/listThreadReducer";
 
 const allReducer = combineReducers({
   listKampus: listKampusReducer,
+  listThread: listThreadReducer
 });
 
 const store = createStore(allReducer, applyMiddleware(thunk))
